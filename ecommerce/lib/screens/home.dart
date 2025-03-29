a2d07425d481c86b9ccf67216cf472e615199b57
Naeem Ahmed <naeemahmedbdn2002@gmail.com>
2025-03-30 02:32:30 +0500

    Add E-commerce UI Project

---
+++ ecommerce/lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    List<Color> Clrs = [
+      Color.fromARGB(255, 246, 111, 58),
+      Color.fromARGB(255, 36, 131, 233),
+      Color.fromARGB(255, 63, 208, 143),
+    ];
+
+    var imgageList = [
+      VIdeo: 9:34
+    ];
+
+    var iconImagesList = [
+
+    ];
+
+    return Container();
+  }
+}
---
+++ ecommerce/lib/screens/screens.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+
+class Screens extends StatefulWidget {
+  const Screens({super.key});
+
+  @override
+  State<Screens> createState() => _ScreensState();
+}
+
+class _ScreensState extends State<Screens> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('Home Screen', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Search Screen', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Notifications', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Profile', style: TextStyle(fontSize: 24))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(0.1))
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey,
+              hoverColor: Colors.blueGrey,
+              gap: 8,
+              activeColor: Colors.white,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+              duration: Duration(microseconds: 400),
+              tabBackgroundColor: Colors.redAccent,
+              tabs: [
+                GButton(icon: LineIcons.home, text: 'Home'),
+                GButton(icon: LineIcons.shoppingBag, text: 'Cart'),
+                GButton(icon: LineIcons.heart, text: 'Wishlist'),
+                GButton(icon: LineIcons.user, text: 'Account'),
+                
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
