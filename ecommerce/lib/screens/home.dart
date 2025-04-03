9ae6bbb54ade514da2a6c93696c387a5fc449139
Naeem Ahmed <naeemahmedbdn2002@gmail.com>
2025-04-03 21:27:27 +0500

    Update Codebase

--- ecommerce/lib/screens/home.dart
+++ ecommerce/lib/screens/home.dart
@@ -5,61 +5,67 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Color> Clrs = [
-      Color.fromARGB(255, 246, 111, 58),
-      Color.fromARGB(255, 36, 131, 233),
-      Color.fromARGB(255, 63, 208, 143),
+      const Color.fromARGB(255, 246, 111, 58),
+      const Color.fromARGB(255, 36, 131, 233),
+      const Color.fromARGB(255, 63, 208, 143),
     ];
 
-    var imgageList = [];
+    var imgageList = ['images/1.png', 'images/2.png'];
 
-    var iconImagesList = [];
+    var iconImagesList = ['images/icons/1.png', 
+                          'images/icons/2.png', 
+                          'images/icons/3.png',
+                          'images/icons/4.png',
+                          'images/icons/5.png',
+                          'images/icons/6.png',
+                          ];
 
     return SingleChildScrollView(
         child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-            padding: EdgeInsets.only(top: 60, left: 15, right: 15),
+            padding: const EdgeInsets.only(top: 60, left: 15, right: 15),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-                  padding: EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                      color: Color(0xFFD4ECFF),
+                      color: const Color(0xFFD4ECFF),
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
-                        BoxShadow(
+                        const BoxShadow(
                           color: Colors.black12,
                           blurRadius: 4,
                           spreadRadius: 2,
                         )
                       ]),
-                  child: Icon(
+                  child: const Icon(
                     CupertinoIcons.cart,
                     size: 28,
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                      color: Color(0xFFD4ECFF),
+                      color: const Color(0xFFD4ECFF),
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
-                        BoxShadow(
+                        const BoxShadow(
                           color: Colors.black12,
                           blurRadius: 4,
                           spreadRadius: 2,
                         )
                       ]),
-                  child: Icon(
+                  child: const Icon(
                     CupertinoIcons.search,
                     size: 28,
                   ),
                 ),
               ],
             )),
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(vertical: 25, horizontal: 15),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -68,53 +74,119 @@ class Home extends StatelessWidget {
                 "Hello Dear",
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 5,),
-              Text("Lets shop something!",
-              style: TextStyle(fontSize: 18, color: Colors.black45),)  
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Lets shop something!",
+                style: TextStyle(fontSize: 18, color: Colors.black45),
+              )
             ],
           ),
         ),
         SingleChildScrollView(
           scrollDirection: Axis.horizontal,
-          padding: EdgeInsets.only(left: 15,),
-          child: Row(children: [
-            for(int i = 0; i<3; i++)
-            Container(
-              margin: EdgeInsets.only(right:10),
-              padding: EdgeInsets.only(left: 10),
-              width: MediaQuery.of(context).size.width / 1.5,
-              height: MediaQuery.of(context).size.height / 5.5,
-              decoration: BoxDecoration(
-                color: Clrs[i],
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+          padding: const EdgeInsets.only(
+            left: 15,
+          ),
+          child: Row(
+            children: [
+              for (int i = 0; i < 2; i++)
+                Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  padding: const EdgeInsets.only(left: 10),
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  height: MediaQuery.of(context).size.height / 5.5,
+                  decoration: BoxDecoration(
+                    color: Clrs[i],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("30% off on Winter Collection", style: TextStyle(fontSize: 22, color: Colors.white),),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        width: 90,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Center(
-                          child: Text("Shop Now",
-                          style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold),
+                      Expanded(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "30% off on Winter Collection",
+                            style: TextStyle(fontSize: 22, color: Colors.white),
                           ),
-                        ),
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 2),
+                            padding: const EdgeInsets.all(10),
+                            width: 90,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Center(
+                              child: Text(
+                                "Shop Now",
+                                style: TextStyle(
+                                    color: Colors.redAccent,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                      Image.asset(
+                        imgageList[i],
+                        width: 100,
+                        height: 180,
                       )
                     ],
-                  ))
-                ],
+                  ),
+                )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Top Categories",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
               ),
-            )
-          ],),
+              Text(
+                "See All",
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 10,
+                    color: Colors.black54),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 10,),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Row(
+              children: [
+                for (int i = 0; i<6; i++)
+                Container(
+                  width: 50,
+                  height: 50,
+                  margin: EdgeInsets.only(right: 5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFD4ECFF),
+
+                  ),
+                  child: Image.asset(iconImagesList[i]),
+                ),
+              ],
+            ),
+          ),
         )
       ],
     ));
